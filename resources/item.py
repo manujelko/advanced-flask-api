41890a5650ef5d7d85ae2d4dfd2ef5d2a1e41802
Manuel Šarić <manujelko@gmail.com>
2022-12-07 15:15:05 +0100

    Refactor resources.

--- resources/item.py
+++ resources/item.py
@@ -1,9 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import (
-    jwt_required,
-    get_jwt,
-    get_jwt_identity,
-)
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from models.item import ItemModel
 
 
@@ -16,7 +12,6 @@ class Item(Resource):
         "store_id", type=int, required=True, help="Every item needs a store_id."
     )
 
-    @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -44,10 +39,6 @@ class Item(Resource):
 
     @jwt_required()
     def delete(self, name):
-        claims = get_jwt()
-        if not claims["is_admin"]:
-            return {"message": "Admin privilege required."}, 401
-
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
@@ -70,25 +61,6 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-    @jwt_required(optional=True)
     def get(self):
-        """
-        Here we get the JWT identity, and then if the user is logged in (we were able to get an identity)
-        we return the entire item list.
-
-        Otherwise we just return the item names.
-
-        This could be done with e.g. see orders that have been placed, but not see details about the orders
-        unless the user has logged in.
-        """
         user_id = get_jwt_identity()
-        items = [item.json() for item in ItemModel.find_all()]
-        if user_id:
-            return {"items": items}, 200
-        return (
-            {
-                "items": [item["name"] for item in items],
-                "message": "More data available if you log in.",
-            },
-            200,
-        )
+        return {"items": [item.json() for item in ItemModel.find_all()]}, 200
